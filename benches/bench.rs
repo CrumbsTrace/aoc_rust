1db01082c6f7ca06acb7179a951e9b67792d8836
CrumbsTrace <45168144+CrumbsTrace@users.noreply.github.com>
2022-04-03 12:57:53 +0200

    Day 13

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,20 +7,34 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("All days");
     group.measurement_time(Duration::from_secs(20));
 
-    group.bench_function("All days", |b| {
-        b.iter(day1::run);
-        b.iter(day2::run);
-        b.iter(day3::run);
-        b.iter(day4::run);
-        b.iter(day5::run);
-        b.iter(day6::run);
-        b.iter(day7::run);
-        b.iter(day8::run);
-        b.iter(day9::run);
-        b.iter(day10::run);
-        b.iter(day11::run);
-        b.iter(day12::run);
-    });
+    // group.bench_function("Day 1", |b| b.iter(day1::run));
+    // group.bench_function("Day 2", |b| b.iter(day2::run));
+    // group.bench_function("Day 3", |b| b.iter(day3::run));
+    // group.bench_function("Day 4", |b| b.iter(day4::run));
+    // group.bench_function("Day 5", |b| b.iter(day5::run));
+    // group.bench_function("Day 6", |b| b.iter(day6::run));
+    // group.bench_function("Day 7", |b| b.iter(day7::run));
+    // group.bench_function("Day 8", |b| b.iter(day8::run));
+    // group.bench_function("Day 9", |b| b.iter(day9::run));
+    // group.bench_function("Day 10", |b| b.iter(day10::run));
+    // group.bench_function("Day 11", |b| b.iter(day11::run));
+    // group.bench_function("Day 12", |b| b.iter(day12::run));
+    group.bench_function("Day 13", |b| b.iter(day13::run));
+
+    // group.bench_function("All days", |b| {
+    //     b.iter(day1::run);
+    //     b.iter(day2::run);
+    //     b.iter(day3::run);
+    //     b.iter(day4::run);
+    //     b.iter(day5::run);
+    //     b.iter(day6::run);
+    //     b.iter(day7::run);
+    //     b.iter(day8::run);
+    //     b.iter(day9::run);
+    //     b.iter(day10::run);
+    //     b.iter(day11::run);
+    //     b.iter(day12::run);
+    // });
 
     group.finish();
     // c.bench_function("test", |b| b.iter(day12::run));
---
+++ src/day13.rs
@@ -0,0 +1,83 @@
+use std::{collections::HashSet, fs};
+
+pub fn run() {
+    let input = fs::read_to_string("inputs/day13.txt").unwrap();
+
+    let mut point_list = HashSet::new();
+    let mut folds = Vec::new();
+
+    input.lines().for_each(|line| {
+        if line.contains("fold") {
+            let split = line.split('=').collect::<Vec<&str>>();
+            if split[0].contains('x') {
+                folds.push(('x', split[1].trim().parse::<i32>().unwrap()));
+            } else {
+                folds.push(('y', split[1].trim().parse::<i32>().unwrap()));
+            }
+        } else if !line.is_empty() {
+            let split = line.split(',').collect::<Vec<&str>>();
+            let x = split[0].trim().parse::<i32>().unwrap();
+            let y = split[1].trim().parse::<i32>().unwrap();
+            point_list.insert(Point { x, y });
+        }
+    });
+    let fold_count = folds.len();
+
+    folds.iter().enumerate().for_each(|(index, (axis, coord))| {
+        point_list = fold_along_axis(*axis, &point_list, *coord);
+
+        if index == 0 {
+            assert!(point_list.len() == 661);
+        } else if index == fold_count - 1 {
+            // print the list of points to the console as a grid of dots
+            let width = point_list.iter().map(|p| p.x).max().unwrap();
+            let height = point_list.iter().map(|p| p.y).max().unwrap();
+            let mut grid = vec![vec![' '; width as usize + 1]; height as usize + 1];
+            point_list.iter().for_each(|point| {
+                grid[point.y as usize][point.x as usize] = '#';
+            });
+            grid.iter().for_each(|_row| {
+                // Print commented away for debugging purposes
+                // println!("{}", row.iter().collect::<String>());
+            });
+        }
+    });
+}
+
+fn fold_along_axis(
+    axis: char,
+    point_list: &HashSet<Point>,
+    fold_coordinate: i32,
+) -> HashSet<Point> {
+    let mut new_point_list = HashSet::new();
+    if axis == 'x' {
+        point_list.iter().for_each(|point| {
+            if point.x < fold_coordinate {
+                new_point_list.insert(*point);
+            } else {
+                new_point_list.insert(Point {
+                    x: fold_coordinate - (point.x - fold_coordinate),
+                    y: point.y,
+                });
+            }
+        });
+    } else {
+        point_list.iter().for_each(|point| {
+            if point.y < fold_coordinate {
+                new_point_list.insert(*point);
+            } else {
+                new_point_list.insert(Point {
+                    x: point.x,
+                    y: fold_coordinate - (point.y - fold_coordinate),
+                });
+            }
+        });
+    }
+    new_point_list
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Point {
+    x: i32,
+    y: i32,
+}
