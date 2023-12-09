74c31c0cdaed3a5ecbe394f329e6389603a32f61
CrumbsTrace <CrumbsTrace@protonmail.com>
2023-12-09 13:37:16 +0100

    Minor cleanup

--- src/aoc2023/day9.rs
+++ src/aoc2023/day9.rs
@@ -18,20 +18,16 @@ pub fn run(input: &str) -> (i64, i64) {
 fn next(seq: &[i64], first: bool) -> i64 {
     let mut ns = vec![if first { seq[0] } else { seq[seq.len() - 1] }];
     let mut seq = seq.to_owned();
-    loop {
+    let mut non_zero = true;
+    while non_zero {
+        non_zero = false;
         let mut diffs = Vec::new();
-        let mut non_zero = false;
         seq.windows(2).for_each(|ns| {
             diffs.push(ns[1] - ns[0]);
-            if ns[1] != 0 {
-                non_zero = true;
-            }
+            non_zero = non_zero || ns[1] - ns[0] != 0;
         });
         ns.push(if first { diffs[0] } else { diffs[diffs.len() - 1] });
         seq = diffs;
-        if !non_zero {
-            break;
-        }
     }
     if first {
         let mut result = 0;
@@ -49,13 +45,13 @@ fn example() {
     let input = "0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45";
-    assert_eq!(run(&input), (114, 2));
+    assert_eq!(run(input), (114, 2));
 }
 
 #[test]
 fn real_input() {
     let input = std::fs::read_to_string("inputs/2023/day9.txt").unwrap();
-    assert_eq!(run(&input), (1992273652, 0));
+    assert_eq!(run(&input), (1992273652, 1012));
 }
 
 #[divan::bench]
