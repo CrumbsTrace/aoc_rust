e7e6108fec6f32ec79aceb451dbb4c3b08aa5341
CrumbsTrace <CrumbsTrace@protonmail.com>
2023-12-10 17:33:05 +0100

    Part 2 of Day 10

--- src/aoc2023/day10.rs
+++ src/aoc2023/day10.rs
@@ -2,39 +2,89 @@ use itertools::Itertools;
 use ndarray::Array2;
 use std::collections::{HashSet, VecDeque};
 
-pub fn run(input: &str) -> (u32, u32) {
+pub fn run(input: &str) -> (usize, usize) {
     let lines = input.lines().map(|l| l.chars().collect_vec()).collect_vec();
-    let map = Array2::from_shape_vec(
-        (lines.len(), lines[0].len()),
-        lines.into_iter().flatten().collect_vec(),
-    )
-    .unwrap();
+    let map = Array2::from_shape_fn((lines[0].len(), lines.len()), |(x, y)| lines[y][x]);
     let start = find_start(&map);
-    let distance_map = flood_fill_distance_map(&map, start);
-    let p1 = distance_map.into_iter().max().unwrap();
-    (p1, 0)
+    let scaled_up_map = scale_up(&map, start);
+    let true_count = scaled_up_map.iter().filter(|b| **b).count();
+    let outside = flood_fill_outside(&scaled_up_map);
+    let inside_count = scaled_up_map
+        .indexed_iter()
+        .filter(|(pos, &v)| !v && !outside.contains(pos) && pos.0 % 2 == 0 && pos.1 % 2 == 0)
+        .count();
+    (true_count / 4, inside_count)
 }
 
-fn flood_fill_distance_map(map: &Array2<char>, start: (usize, usize)) -> Array2<u32> {
-    let mut distance_map = Array2::zeros(map.dim());
+fn flood_fill_outside(map: &Array2<bool>) -> HashSet<(usize, usize)> {
+    let edges = get_outside_edges(map);
+    let mut queue = VecDeque::from(edges);
     let mut visited = HashSet::new();
-    let mut queue = VecDeque::new();
-    queue.push_back(start);
-    visited.insert(start);
-    distance_map[start] = 0;
-    while let Some((x, y)) = queue.pop_front() {
-        let distance = distance_map[[y, x]];
-        let valid_neighbors = valid_neighbors(map, (x, y));
-        for (x2, y2) in valid_neighbors {
-            if visited.contains(&(x2, y2)) {
-                continue;
+    while let Some(pos) = queue.pop_front() {
+        if visited.contains(&pos) {
+            continue;
+        }
+        visited.insert(pos);
+        for dx in -1..=1 {
+            for dy in -1..=1 {
+                let x = pos.0 as i32 + dx;
+                let y = pos.1 as i32 + dy;
+                if x < 0 || y < 0 || x >= map.dim().0 as i32 || y >= map.dim().1 as i32 {
+                    continue;
+                }
+                if map[[x as usize, y as usize]] {
+                    continue;
+                }
+                queue.push_back((x as usize, y as usize));
             }
-            visited.insert((x2, y2));
-            distance_map[[y2, x2]] = distance + 1;
-            queue.push_back((x2, y2));
         }
     }
-    distance_map
+    visited
+}
+
+fn get_outside_edges(map: &Array2<bool>) -> Vec<(usize, usize)> {
+    let mut edges = Vec::new();
+    for x in 0..map.dim().0 {
+        if !map[[x, 0]] {
+            edges.push((x, 0));
+        }
+        if !map[[x, map.dim().1 - 1]] {
+            edges.push((x, map.dim().1 - 1));
+        }
+    }
+    for y in 0..map.dim().1 {
+        if !map[[0, y]] {
+            edges.push((0, y));
+        }
+        if !map[[map.dim().0 - 1, y]] {
+            edges.push((map.dim().0 - 1, y));
+        }
+    }
+    edges
+}
+
+//The that two neighbors get an extra piece inserted in between
+fn scale_up(map: &Array2<char>, start: (usize, usize)) -> Array2<bool> {
+    let mut new_map = Array2::from_elem((map.dim().0 * 2 - 1, map.dim().1 * 2 - 1), false);
+    let mut queue = VecDeque::from(vec![start]);
+    let mut visited = HashSet::new();
+    while let Some(pos) = queue.pop_front() {
+        if visited.contains(&pos) {
+            continue;
+        }
+        visited.insert(pos);
+        let neighbors = valid_neighbors(&map, pos);
+        for (x, y) in neighbors {
+            queue.push_back((x, y));
+            let new_pos = (x * 2, y * 2);
+            new_map[[new_pos.0, new_pos.1]] = true;
+            let dx = pos.0 as i32 - x as i32;
+            let dy = pos.1 as i32 - y as i32;
+            let new_pos = (new_pos.0 as i32 + dx, new_pos.1 as i32 + dy);
+            new_map[[new_pos.0 as usize, new_pos.1 as usize]] = true;
+        }
+    }
+    new_map
 }
 
 fn valid_neighbors(map: &Array2<char>, pos: (usize, usize)) -> Vec<(usize, usize)> {
@@ -49,7 +99,7 @@ fn valid_neighbors(map: &Array2<char>, pos: (usize, usize)) -> Vec<(usize, usize
 }
 
 fn neighbors(map: &Array2<char>, pos: (usize, usize)) -> Vec<(usize, usize)> {
-    let dirs = match map[[pos.1, pos.0]] {
+    let dirs = match map[[pos.0, pos.1]] {
         '|' => vec![(0, 1), (0, -1)],
         '-' => vec![(1, 0), (-1, 0)],
         'L' => vec![(0, -1), (1, 0)],
@@ -73,9 +123,9 @@ fn neighbors(map: &Array2<char>, pos: (usize, usize)) -> Vec<(usize, usize)> {
 }
 
 fn find_start(map: &Array2<char>) -> (usize, usize) {
-    for (y, row) in map.outer_iter().enumerate() {
-        for (x, c) in row.iter().enumerate() {
-            if *c == 'S' {
+    for y in 0..map.dim().1 {
+        for x in 0..map.dim().0 {
+            if map[[x, y]] == 'S' {
                 return (x, y);
             }
         }
@@ -90,13 +140,57 @@ fn example() {
 SJLL7
 |F--J
 LJ.LJ";
-    assert_eq!(run(input), (8, 0));
+    assert_eq!(run(input), (8, 1));
+}
+
+#[test]
+fn example2() {
+    let input = "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
+    assert_eq!(run(input), (80, 10));
+}
+
+#[test]
+fn example3() {
+    let input = "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........";
+    assert_eq!(run(input), (23, 4));
+}
+
+#[test]
+fn example4() {
+    let input = ".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...";
+    assert_eq!(run(input), (70, 8));
 }
 
 #[test]
 fn real_input() {
     let input = std::fs::read_to_string("inputs/2023/day10.txt").unwrap();
-    assert_eq!(run(&input), (7012, 1012));
+    assert_eq!(run(&input), (7102, 323));
 }
 
 #[divan::bench]
