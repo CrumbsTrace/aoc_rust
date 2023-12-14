ea742d0d37ecb9429e68a73856697f5542863090
CrumbsTrace <crumbstrace@protonmail.com>
2023-12-14 21:42:21 +0100

    Ugly but performant rewrite

--- src/aoc2023/day14.rs
+++ src/aoc2023/day14.rs
@@ -1,96 +1,118 @@
-use std::collections::HashSet;
-
 use itertools::Itertools;
+use ndarray::Array2;
 
-pub fn run(input: &str) -> (i32, i32) {
-    let mut square_rocks = HashSet::new();
-    let mut circle_rocks = HashSet::new();
-    let grid = input.lines().collect_vec();
-    let dims = (grid[0].len() as i32, grid.len() as i32);
-    for (y, line) in input.lines().enumerate() {
-        for (x, c) in line.chars().enumerate() {
-            if c == '#' {
-                square_rocks.insert((x as i32, y as i32));
-            } else if c == 'O' {
-                circle_rocks.insert((x as i32, y as i32));
-            }
-        }
-    }
-    for x in 0..dims.0 {
-        square_rocks.insert((x, -1));
-        square_rocks.insert((x, dims.1));
-    }
-    for y in 0..dims.1 {
-        square_rocks.insert((-1, y));
-        square_rocks.insert((dims.0, y));
-    }
-
-    let p1_result = slide(&square_rocks, &circle_rocks, (0, -1));
-    let p1 = p1_result.into_iter().map(|(_, y)| dims.1 - y).sum();
-    let p2_result = slide_n_times(&square_rocks, &circle_rocks, 1000000000);
-    let p2 = p2_result.into_iter().map(|(_, y)| dims.1 - y).sum();
+pub fn run(input: &str) -> (usize, usize) {
+    let grid = input.lines().map(|line| line.as_bytes().to_owned()).collect_vec();
+    let dims = (grid[0].len(), grid.len());
+    let grid = Array2::from_shape_fn(dims, |(x, y)| grid[y][x]);
+    let mut p1_grid = grid.clone();
+    slide_up(&mut p1_grid);
+    let p1 = weight(&p1_grid);
+    let p2_result = slide_n_times(grid, 1000000000);
+    let p2 = weight(&p2_result);
     (p1, p2)
 }
 
-fn slide_n_times(
-    walls: &HashSet<(i32, i32)>,
-    circles: &HashSet<(i32, i32)>,
-    n: usize,
-) -> HashSet<(i32, i32)> {
-    let mut direction = (0, -1);
-    let mut known_states = Vec::new();
-    let mut circles = circles.clone();
-    known_states.push(circles.clone());
-    while known_states.len() < n {
-        for _ in 0..4 {
-            circles = slide(walls, &circles, direction);
-            direction = next_direction(direction);
+fn weight(grid: &Array2<u8>) -> usize {
+    let mut sum = 0;
+    for x in 0..grid.dim().0 {
+        for y in 0..grid.dim().1 {
+            if grid[(x, y)] == b'O' {
+                sum += grid.dim().1 - y;
+            }
         }
+    }
+    sum
+}
 
-        if known_states.contains(&circles) {
+fn slide_n_times(mut grid : Array2<u8>, n: usize) -> Array2<u8> {
+    if n == 0 {
+        return grid;
+    }
+    let mut known_states = Vec::new();
+    known_states.push(grid.clone());
+    while known_states.len() < n {
+        slide_up(&mut grid);
+        slide_left(&mut grid);
+        slide_down(&mut grid);
+        slide_right(&mut grid);
+        if known_states.contains(&grid) {
             break;
         }
-        known_states.push(circles.clone());
+        known_states.push(grid.clone());
     }
 
     let index = known_states
         .iter()
-        .position(|state| state == &circles)
+        .position(|state| state == grid)
         .unwrap();
     let cycle_length = known_states.len() - index;
     let index = (n - index) % cycle_length + index;
     known_states[index].clone()
 }
 
-fn next_direction(direction: (i32, i32)) -> (i32, i32) {
-    match direction {
-        (0, -1) => (-1, 0),
-        (-1, 0) => (0, 1),
-        (0, 1) => (1, 0),
-        (1, 0) => (0, -1),
-        _ => panic!("Invalid direction"),
+fn slide_up(grid: &mut Array2<u8>) {
+    let dims = grid.dim();
+    for x in 0..dims.0 {
+        let mut heighest_y = 0;
+        for y in 0..dims.1 {
+            if grid[(x, y)] == b'#' {
+                heighest_y = y + 1;
+            } else if grid[(x, y)] == b'O' {
+                grid[(x, y)] = b'.';
+                grid[(x, heighest_y)] = b'O';
+                heighest_y += 1;
+            }
+        }
     }
 }
 
-fn slide(
-    walls: &HashSet<(i32, i32)>,
-    circles: &HashSet<(i32, i32)>,
-    direction: (i32, i32),
-) -> HashSet<(i32, i32)> {
-    let mut result = HashSet::new();
-    for (x, y) in circles
-        .iter()
-        .sorted_by_key(|(x, y)| -x * direction.0 + -y * direction.1)
-    {
-        let mut pos = (*x, *y);
-        let mut next_pos = (x + direction.0, y + direction.1);
-        while !walls.contains(&next_pos) && !result.contains(&next_pos) {
-            pos = next_pos;
-            next_pos = (pos.0 + direction.0, pos.1 + direction.1);
+fn slide_left(grid: &mut Array2<u8>) {
+    let dims = grid.dim();
+    for y in 0..dims.1 {
+        let mut leftmost_x = 0;
+        for x in 0..dims.0 {
+            if grid[(x, y)] == b'#' {
+                leftmost_x = x + 1;
+            } else if grid[(x, y)] == b'O' {
+                grid[(x, y)] = b'.';
+                grid[(leftmost_x, y)] = b'O';
+                leftmost_x += 1;
+            }
         }
-        result.insert(pos);
     }
-    result
+}
+
+fn slide_down(grid: &mut Array2<u8>) {
+    let dims = grid.dim();
+    for x in 0..dims.0 {
+        let mut lowest_y = dims.1 - 1;
+        for y in (0..dims.1).rev() {
+            if grid[(x, y)] == b'#' {
+                lowest_y = y.saturating_sub(1)
+            } else if grid[(x, y)] == b'O' {
+                grid[(x, y)] = b'.';
+                grid[(x, lowest_y)] = b'O';
+                lowest_y = lowest_y.saturating_sub(1);
+            }
+        }
+    }
+}
+
+fn slide_right(grid: &mut Array2<u8>) {
+    let dims = grid.dim();
+    for y in 0..dims.1 {
+        let mut rightmost_x = dims.0 - 1;
+        for x in (0..dims.0).rev() {
+            if grid[(x, y)] == b'#' {
+                rightmost_x = x.saturating_sub(1)
+            } else if grid[(x, y)] == b'O' {
+                grid[(x, y)] = b'.';
+                grid[(rightmost_x, y)] = b'O';
+                rightmost_x = rightmost_x.saturating_sub(1);
+            }
+        }
+    }
 }
 
 #[test]
